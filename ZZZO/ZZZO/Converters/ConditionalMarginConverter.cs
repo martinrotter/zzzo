27cc51c5e8c0efb6272df0bba8e3da3b9b03eb4a
Martin Rotter <rotter.martinos@gmail.com>
2024-02-03 12:27:29 +0100

    prace

---
+++ ZZZO/ZZZO/Converters/ConditionalMarginConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ZZZO.Converters
+{
+  [ValueConversion(typeof(bool), typeof(Thickness))]
+  public class ConditionalMarginConverter : IValueConverter
+  {
+    #region Implementace rozhraní
+
+    public object Convert(object value, Type targetType, object parameter,
+      CultureInfo culture)
+    {
+      if (value is bool b)
+      {
+        if (b)
+        {
+          return (Thickness)parameter;
+        }
+      }
+
+      return new Thickness(0);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter,
+      CultureInfo culture)
+    {
+      throw new NotSupportedException();
+    }
+
+    #endregion
+
+    #region Metody
+
+    #endregion
+  }
+}
