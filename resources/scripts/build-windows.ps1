a73e4ebbf73069a99ae83ddc22ff0877cae7ae6f
Martin Rotter <rotter.martinos@gmail.com>
2024-02-15 10:38:08 +0100

    move files

--- resources/scripts/build-windows.ps1
+++ resources/scripts/build-windows.ps1
@@ -1,4 +1,4 @@
-dotnet publish "ZZZO\ZZZO\ZZZO.csproj" -p:PublishProfile=FolderProfile
+dotnet publish "ZZZO\ZZZO\ZZZO.csproj" -p:PublishProfile=FolderProfile -v:diag
 7z a -tzip -mx9 "zzzo-latest.zip" ".\ZZZO\ZZZO\bin\Release\net6.0-windows\publish\win-x86\*"
 
 ls
