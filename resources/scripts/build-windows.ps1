296ea454856b84ee7669eef214efca57784b5abb
Martin Rotter <rotter.martinos@gmail.com>
2024-02-15 11:09:33 +0100

    save

--- resources/scripts/build-windows.ps1
+++ resources/scripts/build-windows.ps1
@@ -1,4 +1,5 @@
-dotnet publish "ZZZO\ZZZO\ZZZO.csproj" -p:PublishProfile=FolderProfile -v:diag
+dotnet publish "ZZZO\ZZZO\ZZZO.csproj" -p:PublishProfile=FolderProfile -v:detailed
+
 7z a -tzip -mx9 "zzzo-latest.zip" ".\ZZZO\ZZZO\bin\Release\net6.0-windows\publish\win-x86\*"
 
 ls
